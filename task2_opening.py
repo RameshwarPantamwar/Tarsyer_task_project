a4e87661d3a91b349a9f08099675a27cef8e832b
Rameshwar Pantamwar <99608868+RameshwarPantamwar@users.noreply.github.com>
2023-01-30 16:56:36 +0530

    Update task2_opening.py

--- task2_opening.py
+++ task2_opening.py
@@ -1,7 +1,7 @@
 import numpy as np
 import imutils
 import cv2
-#reading the input image
+
 img = cv2.imread("j.png")
 kernel = np.ones((5,5), dtype = "uint8")/9
 opening = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
